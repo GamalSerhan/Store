3cac98099a241da97126f6da0805accafc8546f2
GamalSerhan <53912162+GamalSerhan@users.noreply.github.com>
2020-03-03 08:07:33 -0400

    Add files via upload

---
+++ Store/src/ImageInfo.java
@@ -0,0 +1,17 @@
+public class ImageInfo {
+
+	public String path;
+
+	public ImageInfo(String path) {
+		this.path = path;
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public String getImageType() {
+		return path.substring(path.indexOf(".") + 1);
+
+	}
+}
---
+++ Store/src/ProductCategory.java
@@ -0,0 +1,6 @@
+public enum ProductCategory 
+{
+    Accessories,
+    Bikes,
+    Cloathing
+}
